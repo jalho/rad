c6802f7dbb862e75b3a4eecd195e6b143a59e497
jalho <alho.jere@gmail.com>
2024-02-24 18:24:36 +0200

    Format

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 DOWNLOAD_STEAMCMD="https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz"
 DOWNLOAD_MYSTUFF="https://github.com/jalho/DTEK2058-Advanced-Software-Project/releases/download/v0.1.0-alpha/release.tgz"
-INITIAL_WORKINGDIR=$(pwd)
 
+INITIAL_WORKINGDIR=$(pwd)
 echo "DEBUG: INITIAL_WORKINGDIR is $INITIAL_WORKINGDIR"
 
 set -e
@@ -14,12 +14,13 @@ cd /home/rust/
 wget $DOWNLOAD_STEAMCMD
 tar -xzf ./steamcmd_linux.tar.gz
 rm ./steamcmd_linux.tar.gz
+
 wget $DOWNLOAD_MYSTUFF
 tar -xzf ./release.tgz
 rm ./release.tgz
+
 chown -R rust:rust /home/rust/
 chmod u+x ./run-with-carbon.sh
-cd $INITIAL_WORKINGDIR
 
 mv /home/rust/rust.service /etc/systemd/system/
 systemctl daemon-reload

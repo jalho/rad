ac8a7d51e24608c26680e15743bf271c28b0421c
jalho <alho.jere@gmail.com>
2024-02-24 17:57:51 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DOWNLOAD_STEAMCMD="https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz"
+DOWNLOAD_MYSTUFF="https://github.com/jalho/DTEK2058-Advanced-Software-Project/releases/download/v0.1.0-alpha/release.tgz"
+INITIAL_WORKINGDIR=$(pwd)
+
+echo "DEBUG: INITIAL_WORKINGDIR is $INITIAL_WORKINGDIR"
+
+set -e
+
+useradd --create-home --shell /bin/bash --uid 1000 rust
+
+cd /home/rust/
+wget $DOWNLOAD_STEAMCMD
+tar -xzf ./steamcmd_linux.tar.gz
+rm ./steamcmd_linux.tar.gz
+wget $DOWNLOAD_MYSTUFF
+tar -xzf ./release.tgz
+rm ./release.tgz
+chown -R rust:rust /home/rust/
+chmod u+x ./run-with-carbon.sh
+cd $INITIAL_WORKINGDIR
+
+mv /home/rust/rust.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable rust.service
+systemctl start rust.service

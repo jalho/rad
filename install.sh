898d34fd3a13e72a81cf18b35bff45e52ad16c25
jalho <alho.jere@gmail.com>
2024-02-25 00:54:07 +0200

    Install Carbon

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DOWNLOAD_STEAMCMD="https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz"
+DOWNLOAD_CARBONMOD="https://github.com/CarbonCommunity/Carbon.Core/releases/download/production_build/Carbon.Linux.Release.tar.gz"
 DOWNLOAD_MYSTUFF="https://github.com/jalho/DTEK2058-Advanced-Software-Project/releases/download/v0.1.0-alpha/release.tgz"
 
 set -e
@@ -22,6 +23,10 @@ cd /home/rust/
 wget $DOWNLOAD_MYSTUFF
 tar -xzf ./release.tgz
 rm ./release.tgz
+
+wget $DOWNLOAD_CARBONMOD
+tar -xzf ./Carbon.Linux.Release.tar.gz
+rm ./Carbon.Linux.Release.tar.gz
 
 chown -R rust:rust /home/rust/
 chmod u+x ./run-with-carbon.sh

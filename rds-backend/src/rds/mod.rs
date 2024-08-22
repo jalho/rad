39e46cd9e13738f21d537201679a1ef6025f0a4a
jalho <alho.jere@gmail.com>
2024-08-22 15:54:34 +0300

    Remove unnecessary libc::fork

--- rds-backend/src/rds/mod.rs
+++ rds-backend/src/rds/mod.rs
@@ -1,5 +1,9 @@
 pub struct Fork {
+    /// Not the JoinHandle for the RustDedicated game server process, but for
+    /// the OS thread that created that process.
     pub jh: std::thread::JoinHandle<()>,
+
+    /// PID of the process running the RustDedicated game server.
     pub pid: u32,
 }
 
@@ -7,45 +11,30 @@ pub struct Fork {
 pub fn rds_launch_fork() -> Fork {
     let (tx, rx) = std::sync::mpsc::channel::<u32>();
 
-    /*
-      We're calling libc::fork here which duplicates the current thread, so we
-      need a clean thread, therefore std::thread::spawn.
-    */
     let jh = std::thread::spawn(move || {
-        /*
-          PID of the forked process that creates yet another process that in
-          turn runs the RustDedicated game server.
-        */
-        let pid_intermediate: libc::pid_t;
-
         /*
           PID of the RustDedicated game server process.
         */
-        let pid_final: u32;
+        let pid: u32;
 
         let mut rds_command = std::process::Command::new("./RustDedicated");
+
+        /*
+          We don't want to call rds_process.wait() because that would make the
+          parent process wait until the child has exited before continuing and
+          likewise terminate the child upon termination of parent. What we want
+          is separate lifecycles of the processes so one can continue operation
+          while the other is taken down and perhaps restarted.
+        */
         let rds_process: std::process::Child;
         rds_process = rds_command.spawn().unwrap();
-        pid_final = rds_process.id();
-        _ = tx.send(pid_final).unwrap(); // TODO: Handle properly!
 
-        unsafe {
-            /*
-              Duplicate current process into an independent process.
-            */
-            pid_intermediate = libc::fork();
-        }
+        pid = rds_process.id();
+        _ = tx.send(pid).unwrap(); // TODO: Handle properly!
 
-        // case "child": Nothing to do!
-        if pid_intermediate == 0 {
-        }
-        // case "parent": Nothing to do!
-        else {
-        }
+        return;
     });
 
-    let pid_final = rx
-        .recv_timeout(std::time::Duration::from_millis(10))
-        .unwrap(); // TODO: Handle properly!
-    return Fork { jh, pid: pid_final };
+    let pid = rx.recv().unwrap(); // TODO: Handle properly!
+    return Fork { jh, pid };
 }
